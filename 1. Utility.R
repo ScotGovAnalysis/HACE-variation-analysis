9c788b90de40b5a0ea9902adbd2e96cbd1eda149
Thomson K (Kirsty) <Kirsty.Thomson@gov.scot>
2026-06-19 12:08:16 +0100

    Renaming script

---
+++ 1. Utility.R
@@ -0,0 +1,15 @@
+## Utility Script ##
+# Download the most recent publication tables from https://www.gov.scot/collections/health-and-care-experience-survey/ #
+
+#Load required packages#
+library(readxl)
+library(purrr)
+library(dplyr)
+
+# File path to the most recent years data #
+# TO BE UPDATED WHEN STORAGE LOCATION DECIDED #
+file_path_demographics <- "Setup documents - to be deleted once storage location decided/HACE+2025+-+2026+-+All+results+by+Demographic+Characteristic.xlsx"
+file_path_geographies <- "Setup documents - to be deleted once storage location decided/HACE+2025+-+2026+-+All+results+by+Geography.xlsx"
+
+
+
