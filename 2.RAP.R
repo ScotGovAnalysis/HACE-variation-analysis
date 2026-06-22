58993fa86259991001f35c1d3a08b5686f3413e5
Thomson K (Kirsty) <Kirsty.Thomson@gov.scot>
2026-06-22 16:42:20 +0100

    ##

--- 2.RAP.R
+++ 2.RAP.R
@@ -6,15 +6,15 @@
 
 # Once you have run the script "1. Utility.R" next Chose the scripts you would 
 # like to run. To remove any scripts type a # (or ctrl shift c) in front of their name
-
-RunScripts <- c(
-  # "3. Data processing.R",
-  "4. GP Urgent access within 2 days.R"
-  # "5. GP - Care rated as Excellent or Good.R",
-  # "6. GP – informed choice.R",
-  # "7. A&E GP Out of Hours – treatment.R"
-)
-
-# Run all scripts - this will run them in the order of the above list
-lapply(RunScripts, source)
-
+# 
+# RunScripts <- c(
+#   # "3. Data processing.R",
+#   # "4. GP Urgent access within 2 days.R"
+#   # "5. GP - Care rated as Excellent or Good.R",
+#   # "6. GP – informed choice.R",
+#   # "7. A&E GP Out of Hours – treatment.R"
+# )
+# 
+# # Run all scripts - this will run them in the order of the above list
+# lapply(RunScripts, source)
+# 
